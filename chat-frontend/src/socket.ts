d6043f5ba1412656b146bd327ed8fcdc7d5d0365
srsahana <sahana562000@gmail.com>
2025-11-26 16:28:03 +0530

    Update socket.io to use backend URL

--- chat-frontend/src/socket.ts
+++ chat-frontend/src/socket.ts
@@ -1,2 +1,7 @@
 import { io } from "socket.io-client";
-export const socket= io("http://localhost:5000/");
+
+export const socket = io(process.env.REACT_APP_BACKEND_URL, {
+    transports: ["websocket"],
+    autoConnect: false,
+  });
+  
